921b2087ef805546dfc2420c442ba8e6b644b51f
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2024-01-23 05:24:17 +0530

    feat: User credentials set up

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,4 +1,5 @@
 import {https as httpV2} from "firebase-functions/v2";
+//this is a cloud function
 export const helloFireWorld = httpV2.onRequest((request, response)=>{
     response.json({data:'Hello Fire World'
         }
--- frontend/src/screens/AuthScreen.tsx
+++ frontend/src/screens/AuthScreen.tsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
 import {createUserWithEmailAndPassword} from 'firebase/auth'
 import './AuthScreen.css'
-import {auth} from "../config/firebase-config";
+import {auth, db} from "../config/firebase-config";
 import {useNavigate} from "react-router-dom";
+import {doc, setDoc, collection} from "firebase/firestore";
 
 
 
@@ -17,6 +18,11 @@ const AuthScreen= () =>{
            .then(async (userCredentials)=>{
                const user =  userCredentials.user;
                console.log(user);
+
+               await setDoc(doc(collection(db, "users"), userCredentials.user.uid), {
+                   firstname: 'Amandi',
+                   lastname: 'Nimasha'
+               });
                setIsAuthenticated(true)
            })
            .catch((error)=>{

27ae0fbfb815abdc2fb314a6b9ec1154ef551758
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2024-01-22 21:05:06 +0530

    feat: user authentication set up

--- frontend/src/screens/AuthScreen.tsx
+++ frontend/src/screens/AuthScreen.tsx
@@ -1,15 +1,34 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {createUserWithEmailAndPassword} from 'firebase/auth'
 import './AuthScreen.css'
-import {auth} from '../config/firebase-config'
+import {auth} from "../config/firebase-config";
+import {useNavigate} from "react-router-dom";
+
+
+
 const AuthScreen= () =>{
     const [email ,setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const navigate = useNavigate();
     const handleLogin =()=>{
+
        createUserWithEmailAndPassword(auth,email,password)
-           .then(async (userCredentials))
+           .then(async (userCredentials)=>{
+               const user =  userCredentials.user;
+               console.log(user);
+               setIsAuthenticated(true)
+           })
+           .catch((error)=>{
+               throw new Error(`User sign up failed with error ${error}`)
+           });
     };
+
+    useEffect(()=>{
+        if(isAuthenticated){
+            navigate('FirestoreWriteScreen');
+        }
+    },[isAuthenticated,navigate])
     return(
         <div className="auth-container">
             <input
